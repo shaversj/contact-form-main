5f38905f139fcdfaacb83cb19ecb9cdd72b2f6d0
Josh Shavers <shaversj@gmail.com>
2024-08-29 07:53:34 -0500

    Extract label into custom component

---
+++ src/components/Label.tsx
@@ -0,0 +1,23 @@
+type Variant = "text" | "radio" | "checkbox" | "textarea";
+
+const VARIANTS: Record<Variant, string> = {
+  text: "block text-[1.125rem] leading-[150%] text-grey-900",
+  radio: "pl-3 text-[1.125rem] leading-[150%] text-grey-900",
+  checkbox: "pl-3 text-grey-900",
+  textarea: "block text-[1.125rem] leading-[150%] text-grey-900",
+};
+
+type LabelProps = {
+  htmlFor: string;
+  required: boolean;
+  displayName?: string;
+  variant?: Variant;
+};
+
+export default function Label({ htmlFor, required, displayName, variant = "text" }: LabelProps) {
+  return (
+    <label className={VARIANTS[variant]} htmlFor={htmlFor}>
+      {displayName} {required && <span className={"text-green-600"}>*</span>}
+    </label>
+  );
+}
