cacb57d77663b981c65e10241d142647af3bf859
Josh Shavers <shaversj@gmail.com>
2024-09-01 12:41:14 -0500

    Fix radio group error message

--- src/components/RadioGroupErrors.tsx
+++ src/components/RadioGroupErrors.tsx
@@ -1,7 +1,9 @@
 export default function RadioGroupErrors({ errors, name }: { errors: Record<string, any>; name: string }) {
+  console.log(errors);
+
   return (
     (errors as Record<string, any>)[name]?.message && (
-      <p className={"inline-flex pt-[.5rem] leading-[150%] text-red"}>{(errors as Record<string, any>)["message"].message.toString()}</p>
+      <p className={"inline-flex pt-[.5rem] leading-[150%] text-red"}>{(errors as Record<string, any>)[name].message.toString()}</p>
     )
   );
 }
