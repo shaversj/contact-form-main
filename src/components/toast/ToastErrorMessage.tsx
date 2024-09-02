907b8952ab83a1a1622fb9f1092f363dea2ab3dc
Josh Shavers <shaversj@gmail.com>
2024-09-02 09:39:31 -0500

    Fix build errors

--- src/components/toast/ToastErrorMessage.tsx
+++ src/components/toast/ToastErrorMessage.tsx
@@ -7,7 +7,7 @@ export default function ToastErrorMessage() {
         <SuccessCheckSVG />
         <span className={"inline-block text-[1.125rem] font-bold leading-[150%] text-white"}>Message Sent!</span>
       </div>
-      <span className={"inline-block pt-2 leading-[150%] text-green-200"}>Thanks for completing the form. We'll be in touch soon!</span>
+      <span className={"inline-block pt-2 leading-[150%] text-green-200"}>Thanks for completing the form. We&apos;ll be in touch soon!</span>
     </div>
   );
 }
